3b351f4cad665db1d8ceeb47cd1c18d27654c597
mlakowsky <mlakowsky@gmail.com>
2024-01-21 23:20:00 -0300

    first build

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -12,7 +12,7 @@ public class Sound {
     public Sound() {
         soundURL[0] = getClass().getResource("/sounds/BlueBoyAdventure.wav");
         soundURL[1] = getClass().getResource("/sounds/coin.wav");
-        soundURL[2] = getClass().getResource("/sounds/powerUp.wav");
+        soundURL[2] = getClass().getResource("/sounds/powerup.wav");
         soundURL[3] = getClass().getResource("/sounds/unlock.wav");
         soundURL[4] = getClass().getResource("/sounds/fanfare.wav");
     }
